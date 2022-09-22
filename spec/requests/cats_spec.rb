7332ed984cf508df9d15cc57750f3842d08804e4
dnlclmnt <dnlclmnt@gmail.com>
2022-09-22 19:42:42 -0400

    added RSpec tests for CRUD and added CRUD methods in cats controller

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -2,6 +2,48 @@ require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of cats" do
+      Cat.create name:"Henry", age:100, enjoys:"sleeping", image:"www.google.com"
+
+      get '/cats'
+
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(cat.length).to eq 1
+    end
+  end
+  describe "POST /create" do
+    it "creates a cat" do
+      cat_params = {
+        cat: {
+          name: 'Greg',
+          age: 4,
+          enjoys: 'being outside',
+          image: "www.www.www.com"
+        }
+      }
+
+      post '/cats', params: cat_params
+
+      expect(response).to have_http_status(200)
+      cat = Cat.first
+      expect(cat.name).to eq 'Greg'
+    end
+  end
+  describe 'PATCH /update' do
+    before(:each) do
+      @cat = Cat.create(name: "Craig", age: 7, enjoys: "something", image: "website")
+      @cat.update(:name => "Adam")
+      get '/cats'
+      expect(response).to have_http_status(200)
+      expect(@cat).to change {@cat.name}.from("Craig").to("Adam")
+    end
+  end
+  describe 'DELETE /delete' do
+    it 'deletes a cat' do
+    @cat = Cat.create(name: "Craig", age: 7, enjoys: "something", image: "website")
+    @cat.destroy
+    expect(Cat.count).to eq 0
+    end
   end
 end
